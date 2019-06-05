43a04adcdf99bc75222f0c2db152f7feee58181d
vave <fiolekcs@o2.pl>
2019-06-05 15:16:10 +0200

    polish messages

---
+++ src/admin/messages/pl/newsadmin.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+// added translation in 1.0.0-beta5:
+
+    'news' => 'Wiadomości',
+    'news_administrate' => 'Admin',
+    'article' => 'Wpis wiadomości',
+    'article_cat_id' => 'Kategoria',
+    'article_title' => 'Tytuł',
+    'article_text' => 'Treść',
+    'article_image_id' => 'Zdjęcie',
+    'article_timestamp_create' => 'Data',
+    'article_timestamp_display_from' => 'Wyświetl z',
+    'article_timestamp_display_until' => 'Wyświetl aż',
+    'article_is_display_limit' => 'Wyświetlanie limitu',
+    'article_image_list' => 'Lista obrazów',
+    'article_file_list' => 'Lista plików',
+    'article_tag' => 'Tagi',
+    'cat' => 'Kategorie',
+    'cat_title' => 'Tytuł kategorii',
+    'cat_delete_error' => 'Ta kategoria jest używana przez jedną lub wiele wiadomości i nie można jej usunąć.',
+    'cat_title_create_error' => 'Dodaj tytuł kategorii.',
+    'tag' => 'Tagi',
+    'tag_title' => 'Tytuł tagu',
+    'tag_title_create_error' => 'Dodaj tytuł tagu.',
+
+    // 1.0.0
+    
+    'teaser_text' => 'Tekst zwiastuna',
+];
