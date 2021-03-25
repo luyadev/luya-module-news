f62a70b4c374d6daf34eab00676ab91564e59e2c
testt23 <rekstomi@abv.bg>
2021-03-25 16:22:56 +0200

    newsadmin.php
    Transalte Bulgarian

---
+++ src/admin/messages/bg/newsadmin.php
@@ -0,0 +1,30 @@
+<?php
+return [
+// added translation in 1.0.0-beta5:
+     'news' => 'Новини',
+     'news_administrate' => 'Администратор',
+     'article' => 'Новини',
+     'article_cat_id' => 'Категория',
+     'article_title' => 'Заглавие',
+     'article_text' => 'Съдържание',
+     'article_image_id' => 'Изображение',
+     'article_timestamp_create' => 'Дата',
+     'article_image_list' => 'Списък с изображения',
+     'article_file_list' => 'Списък с файлове',
+     'article_tag' => 'Етикет',
+     'cat' => 'Категории',
+     'cat_title' => 'Категория',
+     'cat_delete_error' => 'Тази категория се използва от една или няколко новини и не може да бъде изтрита.',
+     'cat_title_create_error' => 'Моля, добавете заглавие на категория.',
+     'tag' => 'тагове',
+     'tag_title' => 'Етикет',
+     'tag_title_create_error' => 'Моля, добавете заглавие на етикет.',
+    
+// 1.0.0
+    
+     'teaser_text' => 'Текст на тийзър',
+
+// 2.0.0
+
+     'article_is_online' => 'Показване на статия онлайн',
+];
