fa39161a54b11e8058f034b5d9236d98abb3bd2f
Basil Suter <git@nadar.io>
2021-07-07 12:30:52 +0000

    fix safe down migration

--- src/admin/migrations/m220707_122612_is_archived.php
+++ src/admin/migrations/m220707_122612_is_archived.php
@@ -11,6 +11,6 @@ class m220707_122612_is_archived extends Migration
 
     public function safeDown()
     {
-        $this->dropColumn('news_article', 'is_archived', $this->boolean()->defaultValue(false));
+        $this->dropColumn('news_article', 'is_archived');
     }
 }
