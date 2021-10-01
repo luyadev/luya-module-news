8bfe7f33ff51766af578ba0c1760356ac9507184
dennisgon <dennisgon69@hotmail.com>
2021-10-02 05:03:46 +0700

    add translation in indonesian languange

---
+++ src/admin/messages/id/newsadmin.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+// added translation in 1.0.0-beta5:
+
+    'news' => 'Berita',
+    'news_administrate' => 'Administrasi',
+    'article' => 'Entri Berita',
+    'article_cat_id' => 'Kategori',
+    'article_title' => 'Judul',
+    'article_text' => 'Konten',
+    'article_image_id' => 'Gambar',
+    'article_timestamp_create' => 'Tanggal',
+    'article_image_list' => 'Daftar gambar',
+    'article_file_list' => 'Daftar file',
+    'article_tag' => 'Tag',
+    'cat' => 'Kategori',
+    'cat_title' => 'Kategori',
+    'cat_delete_error' => 'Kategori ini digunakan oleh satu atau beberapa berita dan tidak dapat dihapus.',
+    'cat_title_create_error' => 'Silakan tambahkan judul kategori.',
+    'tag' => 'Tag',
+    'tag_title' => 'Tag',
+    'tag_title_create_error' => 'Silakan tambahkan judul tag.',
+    
+// 1.0.0
+    
+    'teaser_text' => 'Teks Teaser',
+
+// 2.0.0
+
+    'article_is_online' => 'Tampilkan artikel daring',
+    
+// 3.1.0
+
+    'preview' => 'Pratinjau',
+
+// 4.0.0
+
+    'article_author' => 'Pengarang',
+    'article_link' => 'Tautan',
+    'article_is_archived' => 'Diarsipkan',
+];
